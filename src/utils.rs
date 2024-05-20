d25de99a572cccc597b17bbf8ccc1306926374b3
Thomas Churchman <thomas@kepow.org>
2024-05-20 15:04:03 +0200

    refactor: fmt

--- src/utils.rs
+++ src/utils.rs
@@ -3,17 +3,13 @@ use std::path::Path;
 
 /// Turn a path into a URL with a given prefix. If a scheme and host is given, the path becomes an
 /// absolute URL.
-pub fn path_to_url(
-    scheme_and_host: Option<&str>,
-    path: impl AsRef<Path>,
-) -> anyhow::Result<String> {
+pub fn path_to_url(scheme_and_host: Option<&str>, path: impl AsRef<Path>) -> anyhow::Result<String> {
     let path = path.as_ref();
 
     // allocate roughly enough for the resulting string
     let mut builder = String::with_capacity(
-        (scheme_and_host.map(|s| s.len() + 1).unwrap_or(0)
-            + path.into_iter().map(|p| p.len()).sum::<usize>())
-        .next_power_of_two(),
+        (scheme_and_host.map(|s| s.len() + 1).unwrap_or(0) + path.into_iter().map(|p| p.len()).sum::<usize>())
+            .next_power_of_two(),
     );
     if let Some(s) = scheme_and_host {
         builder.push_str(s);
@@ -43,11 +39,7 @@ mod test {
             "https://example.com/index.html"
         );
         assert_eq!(
-            path_to_url(
-                Some("https://example.com"),
-                PathBuf::from("nested").join("file.xml")
-            )
-            .unwrap(),
+            path_to_url(Some("https://example.com"), PathBuf::from("nested").join("file.xml")).unwrap(),
             "https://example.com/nested/file.xml"
         );
     }
